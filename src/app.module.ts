4e43c2004bd381cb1c96c83fbb9e976b730009e0
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-22 19:55:11 -0300

    feat: add configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigurationModule } from './commons/config/configuration.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigurationService } from './commons/config/configuration.service';
+
+import configuration from './commons/config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    ConfigurationModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigurationModule],
+      useFactory: async (configurationService: ConfigurationService) => ({
+        type: 'mysql' as const,
+        host: configurationService.databaseHost,
+        port: configurationService.databasePort,
+        username: configurationService.databaseUsername,
+        password: configurationService.databasePassword,
+        database: configurationService.databaseName,
+        entities: [__dirname + '/**/*.entity.js'],
+        synchronize: configurationService.databaseSynchronize,
+        logging: configurationService.databaseLogging,
+      }),
+      inject: [ConfigurationService],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/commons/config/configuration.ts
@@ -0,0 +1,29 @@
+export default () => ({
+  port: parseInt(process.env.PORT) || 3000,
+  reportExpireDays: Number(process.env.REPORT_EXPIRE_DAYS) || 10,
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 3306,
+    username: process.env.DATABASE_USERNAME || 'quakelogparser',
+    password: process.env.DATABASE_PASSWORD || 'quakelogparser',
+    name: process.env.DATABASE_NAME || 'quakelogparser',
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+    logging: process.env.DATABASE_LOGGING === 'true',
+  },
+  auth: {
+    jwt: {
+      secret: process.env.AUTH_JWT_SECRET || 'secretKey',
+      expiresIn: process.env.AUTH_JWT_EXPIRES_IN || '3000',
+    },
+  },
+  logger: {
+    level: process.env.LOGGER_LEVEL || 'debug',
+    label: process.env.LOGGER_LABEL || 'development',
+  },
+  apis: {
+    serasa: {
+      baseUrl: process.env.SERASA_BASE_URL || '',
+      timeout: process.env.SERASA_TIMEOUT || 15000,
+    },
+  },
+});
