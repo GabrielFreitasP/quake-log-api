f9b01da4641b9012fc7db8b88c5b2cc92782cc35
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-22 19:24:34 -0300

    feat: add swagger to start on main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const pckg = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle(pckg.name)
+    .setDescription(pckg.description)
+    .setVersion(pckg.version)
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
