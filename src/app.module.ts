46ef0086f39a08f0b930341b6372e440c50cc536
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-23 18:35:20 -0300

    feat: add data source config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigurationModule } from './commons/config/configuration.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigurationService } from './commons/config/configuration.service';
 import { LoggerModule } from './commons/logger/logger.module';
 import { GameModule } from './game/game.module';
 import { PlayerModule } from './player/player.module';
 import { FilesModule } from './files/files.module';
+import { DataSourceOptions } from '../ormconfig';
 
 import configuration from './commons/config/configuration';
 
@@ -19,21 +19,7 @@ import configuration from './commons/config/configuration';
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     ConfigurationModule,
     TypeOrmModule.forRootAsync({
-      imports: [ConfigurationModule],
-      useFactory: async (configurationService: ConfigurationService) => ({
-        type: 'postgres' as const,
-        host: configurationService.databaseHost,
-        port: configurationService.databasePort,
-        username: configurationService.databaseUsername,
-        password: configurationService.databasePassword,
-        database: configurationService.databaseName,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: configurationService.databaseSynchronize,
-        logging: configurationService.databaseLogging,
-        migrations: ['../migrations/*{.ts,.js}'],
-        migrationsRun: configurationService.databaseMigrationsRun,
-      }),
-      inject: [ConfigurationService],
+      useFactory: async () => DataSourceOptions,
     }),
     LoggerModule,
     GameModule,
