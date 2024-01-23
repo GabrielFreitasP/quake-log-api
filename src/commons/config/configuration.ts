a9f655c76b9322eef6f458027431eca05a83eaa1
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-23 20:40:34 -0300

    feat: add files configurations

--- src/commons/config/configuration.ts
+++ src/commons/config/configuration.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 export default () => ({
   port: parseInt(process.env.PORT) || 3000,
   database: {
@@ -9,6 +11,16 @@ export default () => ({
     synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
     logging: process.env.DATABASE_LOGGING === 'true',
     migrationsRun: process.env.DATABASE_MIGRATIONS_RUN === 'true',
+  },
+  redis: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIST_PORT) || 6379,
+    password: process.env.REDIST_PASSWORD || 'redis2024',
+  },
+  files: {
+    uploadPath: process.env.FILES_UPLOAD_PATH || './uploads',
+    queueName: process.env.FILES_QUEUE_NAME || 'files-queue',
+    jobName: process.env.FILES_JOB_NAME || 'files-job',
   },
   auth: {
     jwt: {
