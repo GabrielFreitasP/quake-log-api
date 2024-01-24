36a5ea816cb13774a274d81d6b8eaff827fc312a
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-24 00:14:53 -0300

    feat: add relations to player entity

--- src/player/entities/player.entity.ts
+++ src/player/entities/player.entity.ts
@@ -1,11 +1,15 @@
 import {
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
-  JoinColumn,
-  ManyToOne,
+  ManyToMany,
+  OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { Game } from '../../game/entities/game.entity';
+import { Kill } from '../../kill/entities/kill.entity';
 
 @Entity('players')
 export class Player {
@@ -15,7 +19,21 @@ export class Player {
   @Column({ name: 'name' })
   name: string;
 
-  @ManyToOne(() => Game, (game) => game.players)
-  @JoinColumn({ name: 'game_id' })
-  game: Game;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+
+  @ManyToMany(() => Game, (game) => game.players)
+  games: Game[];
+
+  @OneToMany(() => Kill, (kill) => kill.killer)
+  kills: Kill[];
+
+  @OneToMany(() => Kill, (kill) => kill.victim)
+  deaths: Kill[];
 }
