9a52eb828532f69743ee097559098f291bfafbe8
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-26 13:32:58 -0300

    feat: add logger to http exception filter

--- src/commons/filters/http-exception.filter.ts
+++ src/commons/filters/http-exception.filter.ts
@@ -3,16 +3,28 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { LoggerService } from '../logger/logger.service';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger: LoggerService) {}
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const exceptionResponse = exception.getResponse() as HttpException;
+    const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    const logMessage = `[${request.method}] ${request.url} (${status})`;
+
+    if (status >= HttpStatus.INTERNAL_SERVER_ERROR) {
+      this.logger.error(logMessage);
+    } else {
+      this.logger.warn(logMessage);
+    }
 
     response.status(status).json({
       statusCode: status,
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggerService } from './commons/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter(app.get(LoggerService)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const options = new DocumentBuilder()
