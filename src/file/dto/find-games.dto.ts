dc22cc2dc1fb242f2d3210ef063949ae1316fc9d
Gabriel Freitas <gabrieldefreitaspinheiro@gmail.com>
2024-01-26 03:41:49 -0300

    feat: rename dtos

--- src/file/dto/find-games.dto.ts
+++
@@ -1,5 +0,0 @@
-import { GameByFileDto } from '../../game/dto/game-by-file.dto';
-
-export interface FindGamesDto {
-  [gameName: string]: GameByFileDto;
-}
--- src/game/dto/game-by-file.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PlayerKillsDto } from '../../kill/dto/player-kills.dto';
-
-export class GameByFileDto {
-  totalKills: number;
-  players: string[];
-  kills: PlayerKillsDto;
-}
